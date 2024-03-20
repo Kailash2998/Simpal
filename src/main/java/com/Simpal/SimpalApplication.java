582732efcdcdaa8983529c74f036f79fcaf9215d
Kailash2998 <105697774+Kailash2998@users.noreply.github.com>
2024-03-20 18:07:29 +0530

    first commit

---
+++ src/main/java/com/Simpal/SimpalApplication.java
@@ -0,0 +1,16 @@
+package com.Simpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpalApplication {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("hello java application");
+		SpringApplication.run(SimpalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Simpal/SimpalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Simpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
